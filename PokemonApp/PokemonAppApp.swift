d07e014b58cb757723795fa4583b23a04de3efa1
Alfredo Fregoso <afregosoa@gmail.com>
2024-12-18 13:27:07 -0600

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Alfredo Fregoso on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
